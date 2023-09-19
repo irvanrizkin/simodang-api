7208b9bd7cdc0f203b75f0d2a2b8cfb7a8bab44d
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-09-19 14:11:02 +0700

    feat(user): find all

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,19 @@ import { NotificationsModule } from './notifications/notifications.module';
 import { MastersModule } from './masters/masters.module';
 import { DevicesModule } from './devices/devices.module';
 import { PondsModule } from './ponds/ponds.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, ArticlesModule, NotificationsModule, MastersModule, DevicesModule, PondsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    ArticlesModule,
+    NotificationsModule,
+    MastersModule,
+    DevicesModule,
+    PondsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
