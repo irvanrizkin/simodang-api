b929f2655ec39d370b632ce5c12dd9862a0591d6
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-09-20 13:09:41 +0700

    fix(user): only admin can list all user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { TokenGuard } from 'src/guard/token.guard';
+import { AdminGuard } from 'src/guard/admin.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @UseGuards(TokenGuard, AdminGuard)
   findAll() {
     return this.usersService.findAll();
   }
