f9a447ef65b0ea437954913a9106bc80fadec833
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-09-22 14:44:40 +0700

    fix(article): add findAll, findOne by admin

--- src/articles/admin.articles.controller.ts
+++ src/articles/admin.articles.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Patch,
   Post,
@@ -13,18 +14,27 @@ import { AdminGuard } from 'src/guard/admin.guard';
 import { ArticlesService } from './articles.service';
 
 @Controller('admin/articles')
+@UseGuards(TokenGuard, AdminGuard)
 export class AdminArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
   @Post()
-  @UseGuards(TokenGuard, AdminGuard)
   create(@Body() createArticleDto: CreateArticleDto) {
     return this.articlesService.create(createArticleDto);
   }
 
   @Patch(':id')
-  @UseGuards(TokenGuard, AdminGuard)
   update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
     return this.articlesService.update(id, updateArticleDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.articlesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.articlesService.findOne(id);
   }
 }
