030789c53ae0b58ffc986ef66b33773a3376bcc9
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-10-06 16:06:56 +0700

    SIMD-160 feat(metric): add admin endpoint for metric device

---
+++ src/metrics/admin.metrics.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import { MetricsService } from './metrics.service';
+import { MetricQueryDto } from './dto/metric-query.dto';
+import { TokenGuard } from 'src/guard/token.guard';
+import { AdminGuard } from 'src/guard/admin.guard';
+
+@Controller('admin/metrics')
+export class AdminMetricsController {
+  constructor(private readonly metricsService: MetricsService) {}
+
+  @Get('/device/:id')
+  @UseGuards(TokenGuard, AdminGuard)
+  findDeviceMetricByHour(
+    @Param('id') id: string,
+    @Query() metricsQueryDto: MetricQueryDto,
+  ) {
+    return this.metricsService.findDeviceMetricByHour(id, metricsQueryDto);
+  }
+}
--- src/metrics/metrics.module.ts
+++ src/metrics/metrics.module.ts
@@ -4,10 +4,11 @@ import { MetricsController } from './metrics.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { DevicesModule } from 'src/devices/devices.module';
 import { LogModule } from 'src/log/log.module';
+import { AdminMetricsController } from './admin.metrics.controller';
 
 @Module({
   imports: [DevicesModule, LogModule],
-  controllers: [MetricsController],
+  controllers: [MetricsController, AdminMetricsController],
   providers: [MetricsService, PrismaService],
   exports: [MetricsService],
 })
