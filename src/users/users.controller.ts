a43b606dd67df1d0b815f4194e17f5a85c2a40ef
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-10-10 20:22:30 +0700

    SIMD-165 feat(swagger): user example response

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,37 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { GuardErrorExample } from 'src/errors/examples/guard-error-example';
 import { TokenGuard } from 'src/guard/token.guard';
 
 @Controller('users')
 @UseGuards(TokenGuard)
 @ApiTags('users')
 @ApiBearerAuth()
+@ApiUnauthorizedResponse({
+  description: 'Unauthorized',
+  content: {
+    'application/json': {
+      examples: {
+        noToken: { value: GuardErrorExample.noToken },
+      },
+    },
+  },
+})
+@ApiForbiddenResponse({
+  description: 'Forbidden',
+  content: {
+    'application/json': {
+      examples: {
+        tokenMismatch: { value: GuardErrorExample.tokenMismatch },
+      },
+    },
+  },
+})
 export class UsersController {
   @Get('/profile')
   getProfile(@Request() req) {
