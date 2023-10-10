eb2b9e5cef5e46229f7e8079178fd8879dd27874
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-10-10 15:04:37 +0700

    SIMD-157 feat(swagger): model for create and update article

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,5 +1,12 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
 export class CreateArticleDto {
+  @ApiPropertyOptional()
   title: string;
+
+  @ApiPropertyOptional()
   url: string;
+
+  @ApiPropertyOptional()
   image: string;
 }
