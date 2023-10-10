65f58f4f08245128d810b09ef676834dea9af1f5
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-10-10 19:49:35 +0700

    SIMD-155 feat(swagger): metric response and model

--- src/metrics/admin.metrics.controller.ts
+++ src/metrics/admin.metrics.controller.ts
@@ -3,12 +3,39 @@ import { MetricsService } from './metrics.service';
 import { MetricQueryDto } from './dto/metric-query.dto';
 import { TokenGuard } from 'src/guard/token.guard';
 import { AdminGuard } from 'src/guard/admin.guard';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { GuardErrorExample } from 'src/errors/examples/guard-error-example';
 
 @Controller('admin/metrics')
 @UseGuards(TokenGuard, AdminGuard)
 @ApiTags('admin/metrics')
 @ApiBearerAuth()
+@ApiUnauthorizedResponse({
+  description: 'Unauthorized',
+  content: {
+    'application/json': {
+      examples: {
+        noToken: { value: GuardErrorExample.noToken },
+      },
+    },
+  },
+})
+@ApiForbiddenResponse({
+  description: 'Forbidden',
+  content: {
+    'application/json': {
+      examples: {
+        tokenMismatch: { value: GuardErrorExample.tokenMismatch },
+        notAdmin: { value: GuardErrorExample.notAdmin },
+      },
+    },
+  },
+})
 export class AdminMetricsController {
   constructor(private readonly metricsService: MetricsService) {}
 
--- src/metrics/dto/metric-query.dto.ts
+++ src/metrics/dto/metric-query.dto.ts
@@ -1,6 +1,15 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 export class MetricQueryDto {
+  @ApiProperty()
   timeString: string;
+
+  @ApiProperty()
   hours: number;
+
+  @ApiPropertyOptional()
   take?: number;
+
+  @ApiPropertyOptional()
   page?: number;
 }
