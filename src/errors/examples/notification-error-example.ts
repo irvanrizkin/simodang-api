ddfe8241fa89a3c698b619edeba94272fefe3b4a
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-10-10 20:06:01 +0700

    SIMD-159 feat(swagger): notification example response and model

---
+++ src/errors/examples/notification-error-example.ts
@@ -0,0 +1,12 @@
+export const NotificationErrorExample = {
+  notFound: {
+    message: 'notification not found',
+    error: 'Not Found',
+    statusCode: 404,
+  },
+  notOwned: {
+    message: 'this notification not yours',
+    error: 'Forbidden',
+    statusCode: 403,
+  },
+};
--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -9,12 +9,40 @@ import {
 } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { TokenGuard } from 'src/guard/token.guard';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { GuardErrorExample } from 'src/errors/examples/guard-error-example';
+import { NotificationErrorExample } from 'src/errors/examples/notification-error-example';
 
 @Controller('notifications')
 @UseGuards(TokenGuard)
 @ApiTags('notifications')
 @ApiBearerAuth()
+@ApiUnauthorizedResponse({
+  description: 'Unauthorized',
+  content: {
+    'application/json': {
+      examples: {
+        noToken: { value: GuardErrorExample.noToken },
+      },
+    },
+  },
+})
+@ApiForbiddenResponse({
+  description: 'Forbidden',
+  content: {
+    'application/json': {
+      examples: {
+        tokenMismatch: { value: GuardErrorExample.tokenMismatch },
+      },
+    },
+  },
+})
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
@@ -37,6 +65,26 @@ export class NotificationsController {
   }
 
   @Patch('/:id')
+  @ApiNotFoundResponse({
+    description: 'Not Found',
+    content: {
+      'application/json': {
+        examples: {
+          masterNotFound: { value: NotificationErrorExample.notFound },
+        },
+      },
+    },
+  })
+  @ApiForbiddenResponse({
+    description: 'Forbidden',
+    content: {
+      'application/json': {
+        examples: {
+          notYours: { value: NotificationErrorExample.notOwned },
+        },
+      },
+    },
+  })
   async updateIsRead(@Param('id') id: string, @Request() req) {
     const { id: userId } = req.user;
 
