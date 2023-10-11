5d3534d5d79a60dd81dbbe1bcfaa2efab28df391
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-10-11 11:10:03 +0700

    SIMD-168 feat(swagger): device example response and model

--- src/devices/dto/update-device.dto.ts
+++ src/devices/dto/update-device.dto.ts
@@ -1,19 +1,46 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateDeviceDto } from './create-device.dto';
 
 export class UpdateDeviceDto extends PartialType(CreateDeviceDto) {
+  @ApiPropertyOptional()
   notificationEnabled?: number;
+
+  @ApiPropertyOptional()
   autoWaterEnabled?: number;
+
+  @ApiPropertyOptional()
   autoFeedEnabled?: number;
+
+  @ApiPropertyOptional()
   isSaved?: number;
+
+  @ApiPropertyOptional()
   tempLow?: number;
+
+  @ApiPropertyOptional()
   tempHigh?: number;
+
+  @ApiPropertyOptional()
   phLow?: number;
+
+  @ApiPropertyOptional()
   phHigh?: number;
+
+  @ApiPropertyOptional()
   tdoLow?: number;
+
+  @ApiPropertyOptional()
   tdoHigh?: number;
+
+  @ApiPropertyOptional()
   tdsLow?: number;
+
+  @ApiPropertyOptional()
   tdsHigh?: number;
+
+  @ApiPropertyOptional()
   turbiditiesLow?: number;
+
+  @ApiPropertyOptional()
   turbiditiesHigh?: number;
 }
