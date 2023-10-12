055f16b89238aa03d303af0a4117a60f8c1589fc
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-10-12 14:31:22 +0700

    SIMD-173 fix(metric): queryString for metric, bad request example response

---
+++ src/errors/examples/metric-error-example.ts
@@ -0,0 +1,10 @@
+export const MetricErrorExample = {
+  badRequest: {
+    message: [
+      'timeString must be a valid ISO 8601 date string',
+      'hours must be a number string',
+    ],
+    error: 'Bad Request',
+    statusCode: 400,
+  },
+};
--- src/metrics/dto/metric-query.dto.ts
+++ src/metrics/dto/metric-query.dto.ts
@@ -1,15 +1,28 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsDateString, IsNumberString, IsOptional } from 'class-validator';
 
 export class MetricQueryDto {
+  @IsDateString()
   @ApiProperty()
   timeString: string;
 
   @ApiProperty()
+  @IsNumberString({
+    no_symbols: true,
+  })
   hours: number;
 
+  @IsNumberString({
+    no_symbols: true,
+  })
+  @IsOptional()
   @ApiPropertyOptional()
   take?: number;
 
+  @IsNumberString({
+    no_symbols: true,
+  })
+  @IsOptional()
   @ApiPropertyOptional()
   page?: number;
 }
