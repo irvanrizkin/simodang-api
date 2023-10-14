5f98a61fcd52b1b6b5feea98644fac6d1da32079
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-10-14 21:27:23 +0700

    SIMD-187 feat(user): update user by token

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUserDto } from './update-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -7,5 +8,12 @@ export class UsersService {
 
   async findAll() {
     return this.prisma.user.findMany();
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+    });
   }
 }
