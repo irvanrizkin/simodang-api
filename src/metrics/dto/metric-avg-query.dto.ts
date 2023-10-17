6a3e4784783a24020873c362d8a8b53ced3822e3
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-10-17 14:13:16 +0700

    SIMD-191 fix(swagger): add description to metric query

--- src/metrics/dto/metric-avg-query.dto.ts
+++ src/metrics/dto/metric-avg-query.dto.ts
@@ -3,24 +3,34 @@ import { IsDateString, IsNumberString, IsOptional } from 'class-validator';
 
 export class MetricAvgQueryDto {
   @IsDateString()
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'the beginning date of the data to be averaged. In YYYY-MM-DD format.',
+  })
   startDate: string;
 
   @IsDateString()
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'the end date of the data to be averaged. In YYYY-MM-DD format.',
+  })
   endDate: string;
 
   @IsNumberString({
     no_symbols: true,
   })
   @IsOptional()
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description: 'Amount of data taken from the database for each page.',
+  })
   take?: number;
 
   @IsNumberString({
     no_symbols: true,
   })
   @IsOptional()
-  @ApiPropertyOptional()
+  @ApiPropertyOptional({
+    description: 'Page number of the data taken from the database.',
+  })
   page?: number;
 }
